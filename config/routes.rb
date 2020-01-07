cd7ec9866329b646c69bb8232af34c4301cfe1ff
kazuya hojo <maboai.kazu29@gmail.com>
2020-01-08 01:39:13 +0900

    支出科目設定機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root "calenders#index"
   resources :users, only: [:edit, :update]
   resources :inputs
+  resources :costs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191229153133) do
+ActiveRecord::Schema.define(version: 20200107115804) do
+
+  create_table "costs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",        null: false
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
